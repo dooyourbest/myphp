3f8fbc0edc44e87b84d98e1e4023ce5036d7360e
zhangsan <772427640@qq.com>
2017-03-21 00:36:15 +0800

    修改部分路由类，提供简单的restful

--- myphp/App.php
+++ myphp/App.php
@@ -6,11 +6,11 @@
  * Date: 2017/3/19
  * Time: 21:31
  */
-use core;
 class App{
   function go(){
       $route=new \core\Route();
-      $route->parse_request();
+      $url=$route->route();
+      dump($url);
   }
 
 }
--- myphp/core/Route.php
+++ myphp/core/Route.php
@@ -3,7 +3,24 @@
 namespace core;
  class Route {
     //分析请求
-     function parse_request(){
-         echo 222;
+    public static function parse_request(){
+        return substr($_SERVER['PATH_INFO'],1);
+    }
+     function restful_config(){
+         return array(
+             'index'=>'Index'
+         );
+     }
+     function route(){
+         $url=self::parse_request();
+         $config=$this->restful_config();
+         if(isset($config[$url])){
+             return explode('/',$config[$url]);
+         }else{
+             return explode('/',$url);
+         }
+     }
+     function dispatch(){
+         
      }
  }
